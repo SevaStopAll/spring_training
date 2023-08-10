53656538b77ff8c1aa2af504a29ddeb42050970d
SevaStopAll <Sevastopallrh@yandex.ru>
2023-08-10 22:15:43 +0400

    Add @Modifying annotation example

--- src/main/java/com/sevastopall/spring/database/repository/UserRepository.java
+++ src/main/java/com/sevastopall/spring/database/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.sevastopall.spring.database.repository;
 
 import com.sevastopall.spring.database.pool.ConnectionPool;
+import com.sevastopall.spring.entity.Role;
 import com.sevastopall.spring.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -18,4 +20,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT u.* FROM users u WHERE u.username = :username",
             nativeQuery = true)
     List<User> findAllByUsername(String username);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("update User u set u.role = :role where u.id in (:ids)")
+    int updateRole(Role role, Long ... ids);
 }
--- src/test/java/com/sevastopall/spring/database/repository/UserRepositoryTest.java
+++ src/test/java/com/sevastopall/spring/database/repository/UserRepositoryTest.java
@@ -1,14 +1,14 @@
 package com.sevastopall.spring.database.repository;
 
+import com.sevastopall.spring.entity.Role;
 import com.sevastopall.spring.entity.User;
 import com.sevastopall.spring.integration.annotation.IT;
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @IT
@@ -20,5 +20,17 @@ class UserRepositoryTest {
     void checkQueries() {
         List<User> users = userRepository.findAllBy("a","ov");
         org.assertj.core.api.Assertions.assertThat(users).hasSize(3);
+    }
+
+    @Test
+    void checkUpdate() {
+        User ivan = userRepository.getById(1L);
+        assertSame(Role.ADMIN, ivan.getRole());
+
+        int resultCount = userRepository.updateRole(Role.USER, 1L, 5L);
+        assertEquals(2, resultCount);
+
+        User sameIvan = userRepository.getById(1L);
+        assertSame(Role.USER, sameIvan.getRole());
     }
 }
