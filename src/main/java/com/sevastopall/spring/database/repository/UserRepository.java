e0953a7768e31b2e5b30ab21e26657049a1f7c39
SevaStopAll <Sevastopallrh@yandex.ru>
2023-08-11 08:52:13 +0400

    Add Pageable and EntityGraph example.

--- src/main/java/com/sevastopall/spring/database/repository/UserRepository.java
+++ src/main/java/com/sevastopall/spring/database/repository/UserRepository.java
@@ -5,12 +5,19 @@ import com.sevastopall.spring.entity.Role;
 import com.sevastopall.spring.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -24,4 +31,14 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query("update User u set u.role = :role where u.id in (:ids)")
     int updateRole(Role role, Long ... ids);
+
+    Optional<User> findTopByOrderByIdDesc();
+
+    List<User> findTop3ByBirthDateBefore(LocalDate birthDate, Sort sort);
+
+    @EntityGraph(attributePaths = {"company", "company.locales"})
+    @Query(value = "select u from User u",
+        countQuery = "select count(distinct u.firstname) from User u")
+    Page<User> findAllBy(Pageable pageable);
+
 }
--- src/test/java/com/sevastopall/spring/database/repository/UserRepositoryTest.java
+++ src/test/java/com/sevastopall/spring/database/repository/UserRepositoryTest.java
@@ -5,8 +5,12 @@ import com.sevastopall.spring.entity.User;
 import com.sevastopall.spring.integration.annotation.IT;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.*;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,5 +36,51 @@ class UserRepositoryTest {
 
         User sameIvan = userRepository.getById(1L);
         assertSame(Role.USER, sameIvan.getRole());
+    }
+
+    @Test
+    void checkFirstTop() {
+        Optional<User> topUser = userRepository.findTopByOrderByIdDesc();
+        assertTrue(topUser.isPresent());
+        topUser.ifPresent(user -> assertEquals(5L, user.getId()));
+    }
+
+    @Test
+    void checkTop3() {
+        List<User> allUsers = userRepository.findTop3ByBirthDateBefore(LocalDate.now(), Sort.by("id").descending());
+        org.assertj.core.api.Assertions.assertThat(allUsers).hasSize(3);
+    }
+
+    @Test
+    void checkSort() {
+        Sort.TypedSort<User> sortBy = Sort.sort(User.class);
+        Sort sortByNames = sortBy.by(User::getFirstname).and(sortBy.by(User::getLastname));
+        List<User> allUsers = userRepository.findTop3ByBirthDateBefore(LocalDate.now(), sortByNames.descending());
+        org.assertj.core.api.Assertions.assertThat(allUsers).hasSize(3);
+    }
+
+    @Test
+    void checkPageable() {
+        PageRequest pageable = PageRequest.of(0, 2, Sort.sort(User.class).by(User::getId));
+        Page<User> slice = userRepository.findAllBy(pageable);
+        slice.forEach(user -> System.out.println(user.getId()));
+
+        while(slice.hasNext()) {
+            slice = userRepository.findAllBy(slice.nextPageable());
+            slice.forEach(user -> System.out.println(user.getId()));
+        }
+    }
+
+    @Test
+    void checkPageableWithEntityGraph() {
+        PageRequest pageable = PageRequest.of(0, 2, Sort.sort(User.class).by(User::getId));
+        Page<User> slice = userRepository.findAllBy(pageable);
+        slice.forEach(user -> System.out.println(user.getCompany().getName()));
+
+        while(slice.hasNext()) {
+            slice = userRepository.findAllBy(slice.nextPageable());
+            slice.forEach(user -> System.out.println(user.getCompany().getName()));
+        }
+        /*org.assertj.core.api.Assertions.assertThat(slice).hasSize(2);*/
     }
 }
