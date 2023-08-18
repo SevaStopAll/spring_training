bac39b4598aa606520f856070a67c2d52a47ac76
SevaStopAll <Sevastopallrh@yandex.ru>
2023-08-18 13:43:25 +0400

    Add validation starter

--- src/main/java/com/sevastopall/spring/dto/UserCreateEditDto.java
+++ src/main/java/com/sevastopall/spring/dto/UserCreateEditDto.java
@@ -5,16 +5,31 @@ import lombok.Value;
 import lombok.experimental.FieldNameConstants;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Value
 @FieldNameConstants
 public class UserCreateEditDto {
+    @Email
     String username;
+
 /*    @DateTimeFormat(pattern = "yyyy-MM-dd")*/
     LocalDate birthDate;
+
+    @NotBlank
+            @Size(min = 3, max = 64)
     String firstname;
+
+    @NotBlank
     String lastname;
+
+    @NotNull
     Role role;
+
+    @NotNull
     Integer companyId;
 }
