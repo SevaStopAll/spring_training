44eefb1f2192fc54485a1a135651030fc27d2269
SevaStopAll <Sevastopallrh@yandex.ru>
2023-08-26 21:22:43 +0400

    Add custom logout page

--- src/main/java/com/sevastopall/spring/config/SecurityConfiguration.java
+++ src/main/java/com/sevastopall/spring/config/SecurityConfiguration.java
@@ -1,10 +1,14 @@
 package com.sevastopall.spring.config;
 
+import com.sevastopall.spring.entity.Role;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -12,12 +16,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests().anyRequest().authenticated()
-                .and()
+                .authorizeHttpRequests(
+                        urlConfig -> urlConfig
+                                .antMatchers("/admin/**").hasAuthority(Role.ADMIN.getAuthority())
+                                .anyRequest().authenticated()
+                )
                 /*.httpBasic(Customizer.withDefaults());*/
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login")
+                        .deleteCookies("JSESSIONID"))
                 .formLogin(login -> login.
                         loginPage("/login")
-                        .defaultSuccessUrl("/users")
-                        .permitAll());
+                        .defaultSuccessUrl("/users"));
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 }
--- src/main/java/com/sevastopall/spring/mapper/UserCreateEditMapper.java
+++ src/main/java/com/sevastopall/spring/mapper/UserCreateEditMapper.java
@@ -5,7 +5,9 @@ import com.sevastopall.spring.dto.UserCreateEditDto;
 import com.sevastopall.spring.entity.Company;
 import com.sevastopall.spring.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
@@ -18,6 +20,7 @@ import static java.util.function.Predicate.*;
 public class UserCreateEditMapper implements Mapper<UserCreateEditDto, User> {
 
     private final CompanyRepository companyRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public User map(UserCreateEditDto fromObject, User toObject) {
@@ -41,6 +44,11 @@ public class UserCreateEditMapper implements Mapper<UserCreateEditDto, User> {
         user.setBirthDate(object.getBirthDate());
         user.setRole(object.getRole());
         user.setCompany(getCompany(object.getCompanyId()));
+
+        Optional.ofNullable(object.getRawPassword())
+                        .filter(StringUtils::hasText)
+                                .map(passwordEncoder::encode)
+                                        .ifPresent(user::setPassword);
 
         Optional.ofNullable(object.getImage())
                 .filter(not(MultipartFile::isEmpty))
